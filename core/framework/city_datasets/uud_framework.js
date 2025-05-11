b4ce2debe3f7307a52f54d45fea33efdf67fb49c
Confoederatio <htmlprogrammingbn@gmail.com>
2025-05-11 15:22:14 +0100

    working on UUD

--- core/framework/city_datasets/uud_framework.js
+++ core/framework/city_datasets/uud_framework.js
@@ -1,6 +1,6 @@
 //Initialise functions
 {
-  function linkChandlerModelskiCities () {
+  global.linkChandlerModelskiCities = function () {
     //Declare local instance variables
     var all_chandler_modelski_cities = Object.keys(main.population.chandler_modelski);
     var uud_obj = JSON.parse(JSON.stringify(main.population.populstat));
@@ -34,15 +34,52 @@
       for (var x = 0; x < local_city_names.length; x++) try {
         local_uud_city = getPopulstatCity(local_city_names[x], { populstat_obj: uud_obj });
 
-        if (local_uud_city) break;
-      } catch (e) {}
+        //Check if .latitude and .longitude are within 1 degree of .coords
+        var latlng = local_uud_city.coords;
+        var ot_latlng = [local_city.latitude, local_city.longitude];
+
+        //Only find cities that are within 1 degree of .coords
+        if (local_uud_city)
+          if (Math.abs(latlng[0] - ot_latlng[0]) <= 1 && Math.abs(latlng[1] - ot_latlng[1]) <= 1) {
+            local_uud_city.break_condition = [local_city_names[x], true];
+            local_uud_city.latlng = latlng;
+            local_uud_city.ot_latlng = ot_latlng;
+            break;
+          }
+        //Reset local_uud_city for next iteration
+        local_uud_city = undefined;
+      } catch (e) {
+        console.error(e);
+      }
 
       if (local_uud_city) {
+        local_uud_city.type = "populstat";
+        local_uud_city.latitude = local_city.latitude;
+        local_uud_city.longitude = local_city.longitude;
         local_uud_city.chandler_modelski_key = all_chandler_modelski_cities[i];
         local_uud_city.chandler_modelski_population = local_city.population;
       } else {
         //Set local_city as a new UUD city
+        local_city.type = "chandler_modelski";
         uud_obj[all_chandler_modelski_cities[i]] = local_city;
+      }
+    }
+
+    //4. Set .type for all remaining UUD cities
+    var all_countries = Object.keys(uud_obj);
+
+    //Iterate over all_countries
+    for (var i = 0; i < all_countries.length; i++) {
+      var local_country = uud_obj[all_countries[i]];
+
+      //Iterate over all_cities
+      var all_cities = Object.keys(local_country);
+
+      for (var x = 0; x < all_cities.length; x++) {
+        var local_city = local_country[all_cities[x]];
+        
+        if (!local_city.type) 
+          local_city.type = "populstat";
       }
     }
 
